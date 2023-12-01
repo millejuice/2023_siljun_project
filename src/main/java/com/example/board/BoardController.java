3d52d08c8f53029d0e7610cf232c4142176bae5e
millejuice <potgusdavy@gmail.com>
2023-12-01 17:20:19 +0900

    list부분 추가

--- src/main/java/com/example/board/BoardController.java
+++ src/main/java/com/example/board/BoardController.java
@@ -1,4 +1,21 @@
 package com.example.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
 public class BoardController {
+    @Autowired
+    BoardService boardService;
+
+    @RequestMapping("/board/list")
+    public String boardList(Model model) {
+        List<BoardVO> list = boardService.getBoardList();
+        model.addAttribute("list", list);
+        return "board/list";
+    }
 }
